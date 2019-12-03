dd2f6f9164718796595c500c0b4e18264651d5e0
Lugaga <maureezgaga@gmail.com>
2019-12-03 10:54:29 +0300

    added a formular to the function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
  function checkDayOfWeek() {
      var year, month, day;
      var male, female;
+
+     year = perseInt(document.getElementById("year").value);
+     month = perseInt(document.getElementById("month").value);
+     day = perseInt(document.getElementById("day".value));
+
+     male = document.getElementById("radio1").checked;
+     female = document.getElementById("radio2").checked;
+
+     var centuary = parseint((year + 1) / 100 + 1);
+     var formula = (((26 * (month + 1) / 10)) + ((5 * year / 4)) + ((century / 4) - 2 * century - 1) + day) % 7;
+     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+     var femaleNames = ["Akosua", "Adowa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+     var daysOfTheweek = ["sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satarday"];
+     var dayByIndex =
+ }
