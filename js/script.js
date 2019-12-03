3ec6233634111de36e87b2a62f36d38ecc1b6a56
Lugaga <maureezgaga@gmail.com>
2019-12-03 12:22:55 +0300

    added bag on the readme.md

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@
      var year, month, day;
      var male, female;
 
-     year = perseInt(document.getElementById("year").value);
-     month = perseInt(document.getElementById("month").value);
-     day = perseInt(document.getElementById("day".value));
+     year = parseInt(document.getElementById("year").value);
+     month = parseInt(document.getElementById("month").value);
+     day = parseInt(document.getElementById("day").value);
 
      male = document.getElementById("radio1").checked;
      female = document.getElementById("radio2").checked;
 
-     var centuary = parseint((year + 1) / 100 + 1);
-     var formula = (((26 * (month + 1) / 10)) + ((5 * year / 4)) + ((century / 4) - 2 * century - 1) + day) % 7;
+     var century = parseInt((year + 1) / 100 + 1);
+     var fomular = (((26 * (month + 1) / 10)) + ((5 * year / 4)) + ((century / 4) - 2 * century - 1) + day) % 7;
      var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
      var femaleNames = ["Akosua", "Adowa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-     var daysOfTheweek = ["sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satarday"];
-     var indexByDate = Math.floor(formula);
+     var daysOfTheWeek = ["sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satarday"];
+     var indexDate = Math.floor(fomular);
 
-     if (year < 0 && year > 2022) {
+     if (year > 0 && year < 2022) {
          if (month > 0 && month < 13) {
              if (day > 0 && day < 32) {
 
                  if (male) {
-                     document.getElementById("result").innerHtml("Your Akan name is" + maleNames[indexByDate] + "you were born on a" [daysOfTheweek][indexByDate]);
+                     document.getElementById("result").innerHtml = ("Your Akan name is" + maleNames[indexDate] + "you were born on a" + daysOfTheWeek[indexByDate]);
                  } else {
-                     document.getElementById("result").innerHtml("Your Akan name is" + femaleNames[indexByDate] + "you were born on a" [daysOfTheweek][indexByDate]);
+                     document.getElementById("result").innerHtml = ("Your Akan name is" + femaleNames[indexDate] + "you were born on a" + daysOfTheWeek[indexByDate]);
                  }
 
              } else {
